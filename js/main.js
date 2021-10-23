edd3c6b5664ccc2d6fb60e0ef1862734361b2575
godwin opara <godwinopara62@yahoo.com>
2021-10-23 18:44:36 +0400

    added javascript to toggle the display of the nav menu when the hamburger is clicked

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
+const hamburger = document.querySelector(".hamburger");
+const hamburgerClose = document.querySelector(".hamburger_close");
+const hamburgerOpen = document.querySelector(".hamburger_open");
+const nav = document.querySelector("nav");
+const overlay = document.querySelector(".overlay");
 
+let navOpen = false;
 
+hamburger.addEventListener("click", () => {
+  navOpen = !navOpen;
+
+  if (navOpen === true) {
+    hamburgerClose.classList.add("show-nav");
+    hamburgerOpen.classList.remove("show-nav");
+    nav.classList.add("show-nav");
+    overlay.classList.add("display-overlay");
+    document.documentElement.style.overflowY = "hidden";
+  } else {
+    hamburgerOpen.classList.add("show-nav");
+    hamburgerClose.classList.remove("show-nav");
+    nav.classList.remove("show-nav");
+    overlay.classList.remove("display-overlay");
+    document.documentElement.style.overflowY = "scroll";
+  }
+});
